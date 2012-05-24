5ddc2e36c6ce81ad73c52215de9421fa1bd37547
Joshua 'Boboon' Holland <boboon06@gmail.com>
2012-05-24 12:46:54 +1200

    Added Panorama Generator... WIP and should be finished hours after AZN gives me some PHOTOS!
    Signed-off-by: Joshua 'Boboon' Holland <boboon06@gmail.com>

---
+++ AutoGenPano/AutoGenPano/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AutoGenPano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(12, 12);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(571, 238);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "Go.";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 262);
+            this.Controls.Add(this.buttonGo);
+            this.Name = "Form1";
+            this.Text = "AutoPano";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+    }
+}
+
---
+++ AutoGenPano/AutoGenPano/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AutoGenPano
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string imagepath = @"D:\Joshua\Waikato University\COMP 241\DIY Street View\Images";
+        private string libpanopath = @"D:\Joshua\Waikato University\COMP 241\DIY Street View\PanoTools\PanoTools";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader csvinput = new StreamReader(imagepath + "\\imagelog.csv");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Because Chris hasn't done the RIG yet, I can waste time like this!
+            this.Text = "AutoPano Creator";
+            if (Environment.MachineName == "RAINBOWDASH" && Environment.UserName == "Boboon")
+            {
+                this.Text += " - rainbowdash.theboboon.com";
+            }
+        }
+    }
+}
